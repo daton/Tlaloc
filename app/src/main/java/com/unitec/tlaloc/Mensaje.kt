a0e6b61847fd97e02ba8e30093f2ea69ba994c78
daton <rapidclimate@gmail.com>
2021-03-23 12:45:11 -0600

    orimero

--- app/src/main/java/com/unitec/tlaloc/Mensaje.kt
+++ app/src/main/java/com/unitec/tlaloc/Mensaje.kt
@@ -1,5 +1,8 @@
 package com.unitec.tlaloc
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Mensaje {
     var titulo:String?=null
     var cuerpo:String?=null
